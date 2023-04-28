a42e32b101f4b2437b926a17dd9a20836fd5c56d
dima_dencep <dima_dencep@constructlegacy.ru>
2023-04-29 04:29:45 +0700

    Fix listening `RegisterClientReloadListenersEvent`

--- src/main/java/ladysnake/satin/Satin.java
+++ src/main/java/ladysnake/satin/Satin.java
@@ -20,8 +20,10 @@ package ladysnake.satin;
 import ladysnake.satin.impl.ReloadableShaderEffectManager;
 import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apiguardian.api.API;
@@ -44,9 +46,11 @@ public class Satin {
     }
 
     public Satin() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
         MinecraftForge.EVENT_BUS.addListener(ReloadableShaderEffectManager.INSTANCE::onResolutionChanged);
 
-        MinecraftForge.EVENT_BUS.register(this);
+        bus.addListener(this::onRegisterReloadEvent);
     }
 
     @SubscribeEvent
